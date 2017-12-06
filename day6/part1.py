7a5ecb1cee3e84dacf3750f297829f3f54ae6a9f
Daniele Venier <danielevenier1998@gmail.com>
2017-12-06 12:37:21 +0100

    Day 6

---
+++ day6/part1.py
@@ -0,0 +1,12 @@
+banks = [int(e) for e in open('day6.input').read().rstrip().split()]
+
+mem = {}
+time = 0
+while repr(banks) not in mem:
+    mem[repr(banks)] = True
+    ind = banks.index(max(banks))
+    banks[ind], val = 0, banks[ind]
+    while val:
+        banks[(ind+val)%len(banks)], val = banks[(ind+val)%len(banks)]+1, val-1
+    time += 1
+print time
