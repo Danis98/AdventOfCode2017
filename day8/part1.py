f15d4922f81f169738688cdfa423bca475566263
Daniele Venier <danielevenier1998@gmail.com>
2017-12-08 11:11:04 +0100

    Day 8

---
+++ day8/part1.py
@@ -0,0 +1,43 @@
+instrs = open('day8.input').read().rstrip().split('\n')
+
+regs = {}
+
+for instr in instrs:
+    words = instr.split()
+    reg_id = words[0]
+    op = words[1]
+    val = int(words[2])
+    cmp_reg = words[4]
+    cmp = words[5]
+    cmp_val = int(words[6])
+
+    if reg_id not in regs:
+        regs[reg_id] = 0
+    if cmp_reg not in regs:
+        regs[cmp_reg] = 0
+
+    if cmp == '==':
+        if regs[cmp_reg] != cmp_val:
+            continue
+    elif cmp == '!=':
+        if regs[cmp_reg] == cmp_val:
+            continue
+    elif cmp == '>=':
+        if regs[cmp_reg] < cmp_val:
+            continue
+    elif cmp == '<=':
+        if regs[cmp_reg] > cmp_val:
+            continue
+    elif cmp == '>':
+        if regs[cmp_reg] <= cmp_val:
+            continue
+    elif cmp == '<':
+        if regs[cmp_reg] >= cmp_val:
+            continue
+
+    if op == 'inc':
+        regs[reg_id] += val
+    if op == 'dec':
+        regs[reg_id] -= val
+
+print max(regs.values())
