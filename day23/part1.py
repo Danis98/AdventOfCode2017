0b7f33c6b4838b7f607997ded122eab3391c3ada
Daniele Venier <danielevenier1998@gmail.com>
2017-12-24 08:25:53 +0100

    Day 23

---
+++ day23/part1.py
@@ -0,0 +1,35 @@
+import time
+
+instrs = open('day23.input').read().rstrip().split('\n')
+
+regs = {}
+instr_ptr = 0
+mul_cnt = 0
+
+def get_val(tok):
+    if (tok[0]>='0' and tok[0]<='9') or tok[0] == '-':
+        return int(tok)
+    else:
+        if tok not in regs:
+            regs[tok] = 0
+        return regs[tok]
+
+while instr_ptr < len(instrs):
+    ops = instrs[instr_ptr].split()
+    if ops[0] == 'set':
+        regs[ops[1]] = get_val(ops[2])
+    elif ops[0] == 'sub':
+        if ops[1] not in regs:
+            regs[ops[1]] = 0
+        regs[ops[1]] -= get_val(ops[2])
+    elif ops[0] == 'mul':
+        mul_cnt += 1
+        if ops[1] not in regs:
+            regs[ops[1]] = 0
+        regs[ops[1]] *= get_val(ops[2])
+    elif ops[0] == 'jnz':
+        if get_val(ops[1]) != 0:
+            instr_ptr += get_val(ops[2]) - 1
+    instr_ptr += 1
+
+print mul_cnt
