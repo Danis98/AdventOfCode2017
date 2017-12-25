df719ae684d62c73cb21aef94fc58b7c7e4410e8
Daniele Venier <danielevenier1998@gmail.com>
2017-12-25 10:39:59 +0100

    Final cleanup

--- day23/part2.py
+++ day23/part2.py
@@ -1,35 +1,16 @@
-import time
+from math import sqrt
 
-instrs = open('day23.input').read().rstrip().split('\n')
+lower = 108100
+upper = lower + 17000
 
-regs = {
-    'a': 1
-}
-instr_ptr = 0
+def is_prime(num):
+    for i in xrange(2, int(sqrt(num)+.5)):
+        if num % i == 0:
+            return False
+    return True
 
-def get_val(tok):
-    if (tok[0]>='0' and tok[0]<='9') or tok[0] == '-':
-        return int(tok)
-    else:
-        if tok not in regs:
-            regs[tok] = 0
-        return regs[tok]
-
-while instr_ptr < len(instrs):
-    ops = instrs[instr_ptr].split()
-    if ops[0] == 'set':
-        regs[ops[1]] = get_val(ops[2])
-    elif ops[0] == 'sub':
-        if ops[1] not in regs:
-            regs[ops[1]] = 0
-        regs[ops[1]] -= get_val(ops[2])
-    elif ops[0] == 'mul':
-        if ops[1] not in regs:
-            regs[ops[1]] = 0
-        regs[ops[1]] *= get_val(ops[2])
-    elif ops[0] == 'jnz':
-        if get_val(ops[1]) != 0:
-            instr_ptr += get_val(ops[2]) - 1
-    instr_ptr += 1
-
-print regs
+cnt = 0
+for num in xrange(lower, upper+1, 17):
+    if not is_prime(num):
+        cnt += 1
+print cnt
